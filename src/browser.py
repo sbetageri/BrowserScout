5f8cf42526030e08a7826d39bd55226460cad61b
Srirakshith Betageri <sbetageri111@gmail.com>
2025-05-27 00:31:14 +0530

    Add browser and agent

--- src/browser.py
+++ src/browser.py
@@ -1,9 +1,13 @@
+import io
+import base64
 import asyncio
 
-from playwright.async_api import async_playwright, playwright
+from PIL import Image
+from playwright.async_api import async_playwright
 
 import utils
 import constants
+
 
 class Browser:
     CLICKABLE_ITEMS = {
@@ -21,9 +25,10 @@ class Browser:
         constants.SEARCH,
         constants.SEARCH_BOX,
         constants.SPIN_BUTTON,
-        constants.TEXTBOX
+        constants.TEXTBOX,
     }
-    def __init__(self, headless: bool=True):
+
+    def __init__(self, headless: bool = True):
         self.playwright = None
         self.browser = None
         self.headless = headless
@@ -43,23 +48,42 @@ class Browser:
         :return:
         """
         if self.browser is not None:
-            self.browser.close()
+            await self.browser.close()
 
         if self.playwright is not None:
-            self.playwright.stop()
+            await self.playwright.stop()
 
     async def open_page(self, url: str):
         """Open the given page"""
         self.opened_page = await self.browser.new_page()
         await self.opened_page.goto(url)
 
-    async def find_all_input_locators(self, url: str=None):
+    async def _find_all_clickable_locators(self):
         """Identify all locators in the page that correspond to an input element"""
-        if url is not None:
-            await self.open_page(url)
+        input_locators = []
+        if self.opened_page is None:
+            raise ValueError("No open page available")
 
-        
+        for clickable_item in Browser.CLICKABLE_ITEMS:
+            locator = self.opened_page.get_by_role(clickable_item).filter(visible=True)
+            input_locators.append(locator)
 
+        return input_locators
+
+    async def _get_screenshot(self) -> Image:
+        """Save screenshot at the path provided"""
+        screenshot_bytes = await self.opened_page.screenshot()
+        image_bytes = base64.b64decode(screenshot_bytes)
+        img = Image.open(io.BytesIO(image_bytes))
+        return img
+
+    async def _get_all_locatos_in_screen(self):
+        """Get all the locators visible in the viewport"""
+        screenshot = await self._get_screenshot()
+        clickable_locators = await self._find_all_clickable_locators()
+        bbox_values = []
+        for locator in clickable_locators:
+            bbox = await locator.bounding_box()
 
 
 async def main():
@@ -69,12 +93,14 @@ async def main():
         await browser.setup_playwright_browser()
         utils.print("Browser setup complete")
         await browser.open_page("https://www.google.com")
+        await browser._find_all_clickable_locators()
         while True:
             continue
-    except:
+    except Exception as ex:
+        utils.print(ex)
         utils.print("Exiting")
     finally:
-        browser.cleanup()
+        await browser.cleanup()
 
 
 if __name__ == "__main__":
